97fe71cdc6d69b7b7ecc9b52b1d02ac4c3050925
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-05-11 14:01:38 -0300

    just testing for feedback

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helmut::Application.config.secret_token = '9cd462b890a89f16691831d52933a2ccfc917a4f35e1719b9966696c97971ed0adc716dea224c3545d7652cb27aaf46973c74c615e38ee7bf17f29c7a9adb31d'
